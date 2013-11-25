18b0b06a8a16e0964836fcf9e80da8c33f0cb599
cztchoice <cztchoice@gmail.com>
2013-11-25 10:46:10 +0800

    The least usable edition, with bugs

--- bootstrap.js
+++ bootstrap.js
@@ -5,25 +5,6 @@ const WM = Cc['@mozilla.org/appshell/window-mediator;1'].
            getService(Ci.nsIWindowMediator);
 //Cu.import("resource://gre/modules/Services.jsm");
 
-//mainWindow.gBrowser.addTab(...);
-
-
-var  dbcloseListener : function(event) {
-  if (event.button != 0) {
-      return;
-  }
-  if (event.target.tagName != "tab") {
-      return;
-  }
-  var tab = event.target;
-  if(tab && !tab.pinned) { // app tabs won't be closed
-    // {animate: true} is actual since Firefox 4
-    // that animation is managed via about:config (browser.tabs.animate)
-    gBrowser.removeTab(tab, {animate: true});
-  }
-};
-
-
 function startup() {
   var window = WM.getMostRecentWindow("navigator:browser");
   var gBrowser = window.gBrowser;
@@ -42,11 +23,8 @@ function startup() {
         gBrowser.removeTab(tab, {animate: true});
       }
   }, false );
-
-
-
-  
 }
+
 function shutdown() {
   var window = WM.getMostRecentWindow("navigator:browser");
   var gBrowser = window.gBrowser;
